2f1e0dc914be17da50c39024342ce0d78c12d5a1
anya <riidloa@gmail.com>
2023-10-30 17:26:09 +0700

    make toggle favourite shortcut replace more menu dialog on detail activity and replace the more menu beside title, add empty rating activity

--- app/src/main/java/com/example/gorenganindonesia/Activity/DetailActivity.java
+++ app/src/main/java/com/example/gorenganindonesia/Activity/DetailActivity.java
@@ -1,17 +1,12 @@
 package com.example.gorenganindonesia.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,19 +21,15 @@ import com.example.gorenganindonesia.ui.Fragments.Detail.IngredientsFragment;
 import com.example.gorenganindonesia.ui.Fragments.Detail.StepsFragment;
 import com.example.gorenganindonesia.ui.Fragments.Detail.SummaryFragment;
 import com.example.gorenganindonesia.Model.data.Receipt.Receipt;
-import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class DetailActivity extends AppCompatActivity {
     TextView tvTitleRingkasan, tvTitleLangkah, tvTitleBahan;
     ImageView ivThumb;
-    ImageButton btnBack, btnMore;
+    ImageButton btnBack, btnToggleFavourite;
     ViewPager vp;
-
-    Button btnShare, btnAddFavourite, btnSaveOffline, btnSeeUserRating;
 
     @SuppressLint("NonConstantResourceId")
     @Override
@@ -52,8 +43,8 @@ public class DetailActivity extends AppCompatActivity {
 
         ivThumb = (ImageView) findViewById(R.id.iv_thumb_detail);
 
-        btnBack = (ImageButton) findViewById(R.id.btn_close_detail);
-        btnMore = (ImageButton) findViewById(R.id.btn_more_detail);
+        btnBack = (ImageButton) findViewById(R.id.ib_close_detail);
+        btnToggleFavourite = (ImageButton) findViewById(R.id.ib_toggle_favourite);
 
 
         vp = (ViewPager) findViewById(R.id.vp_detail);
@@ -65,70 +56,29 @@ public class DetailActivity extends AppCompatActivity {
 
         btnBack.setOnClickListener(v -> onBackPressed());
 
-        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
-        bottomSheetDialog.setContentView(R.layout.bottom_dialog_menu_detail);
-
-        btnShare = (Button) bottomSheetDialog.findViewById(R.id.btn_share);
-        btnAddFavourite = (Button) bottomSheetDialog.findViewById(R.id.btn_add_to_favourite);
-        btnSaveOffline = (Button) bottomSheetDialog.findViewById(R.id.btn_save_offline);
-        btnSeeUserRating = (Button) bottomSheetDialog.findViewById(R.id.btn_see_user_rating);
-
         FavouriteViewModel favViewModel = ((GlobalModel) getApplication()).getFavouriteViewModel();
 
         final boolean[] isReceiptExistInFav = {false};
 
-        btnMore.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                bottomSheetDialog.show();
+        if(!favViewModel.ifFavouriteExist(receipt)){
+            btnToggleFavourite.setImageResource(R.drawable.ic_favourite_outline);
+        } else {
+            btnToggleFavourite.setImageResource(R.drawable.ic_favourite_solid);
+            isReceiptExistInFav[0] = true;
+        }
 
-                if (favViewModel.ifFavouriteExist(receipt)) {
-                    btnAddFavourite.setText("Hapus dari Favorit");
-                    btnAddFavourite.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_cross, 0, 0, 0);
-
-                    isReceiptExistInFav[0] = true;
-                } else {
-                    btnAddFavourite.setText("Tambahkan ke Favorit");
-                    btnAddFavourite.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_love, 0, 0, 0);
-
-                    isReceiptExistInFav[0] = false;
-                }
-            }
-        });
-
-        btnAddFavourite.setOnClickListener(v -> {
+        btnToggleFavourite.setOnClickListener(v -> {
             if(isReceiptExistInFav[0]){
                 favViewModel.removeFavourite(receipt);
                 new CustomToast("Berhasil menghapus dari Favorit", v).show();
+                btnToggleFavourite.setImageResource(R.drawable.ic_favourite_outline);
+                isReceiptExistInFav[0] = false;
             } else {
                 favViewModel.pushFavourite(receipt);
                 new CustomToast("Berhasil menambahkan ke Favorit!", v).show();
+                btnToggleFavourite.setImageResource(R.drawable.ic_favourite_solid);
+                isReceiptExistInFav[0] = true;
             }
-            bottomSheetDialog.dismiss();
-        });
-
-        btnShare.setOnClickListener(v -> {
-            // Create an Intent with the ACTION_SEND action
-            Intent shareIntent = new Intent(Intent.ACTION_SEND);
-
-            shareIntent.setType("text/plain");
-
-            String shareText = receipt.toString();
-            shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
-
-            startActivity(Intent.createChooser(shareIntent, "Bagikan melalui"));
-            bottomSheetDialog.dismiss();
-        });
-
-
-        btnSaveOffline.setOnClickListener(v -> {
-            Toast.makeText(this, "Save Offline clicked", Toast.LENGTH_SHORT).show();
-            bottomSheetDialog.dismiss();
-        });
-
-        btnSeeUserRating.setOnClickListener(v -> {
-            Toast.makeText(this, "See User Rating clicked", Toast.LENGTH_SHORT).show();
-            bottomSheetDialog.dismiss();
         });
 
 
--- app/src/main/java/com/example/gorenganindonesia/ui/Fragments/Detail/SummaryFragment.java
+++ app/src/main/java/com/example/gorenganindonesia/ui/Fragments/Detail/SummaryFragment.java
@@ -1,6 +1,7 @@
 package com.example.gorenganindonesia.ui.Fragments.Detail;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,15 +10,24 @@ import androidx.viewpager.widget.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.gorenganindonesia.Activity.RatingActivity;
+import com.example.gorenganindonesia.Model.GlobalModel;
+import com.example.gorenganindonesia.Model.ViewModel.FavouriteViewModel;
 import com.example.gorenganindonesia.Model.data.Receipt.Receipt;
 import com.example.gorenganindonesia.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 
 public class SummaryFragment extends Fragment {
     TextView tvTitle, tvCategory, tvDifficulty, tvTime, tvPortion, tvStep, tvIngridient;
+    ImageButton ibMore;
+    Button btnShare, btnSaveOffline, btnSeeUserRating;
     LinearLayout llSteps, llIngridients;
     ViewPager vp;
     Receipt receipt;
@@ -52,6 +62,8 @@ public class SummaryFragment extends Fragment {
         llSteps = (LinearLayout) view.findViewById(R.id.ll_steps_detail);
         llIngridients = (LinearLayout) view.findViewById(R.id.ll_ingredients_detail);
 
+        ibMore = (ImageButton) view.findViewById(R.id.ib_more_detail);
+
         tvTitle.setText(receipt.getTitle().toString());
         tvCategory.setText("Kategori " + receipt.getCategory().toString());
         tvDifficulty.setText(receipt.getDifficulty().toString());
@@ -72,6 +84,41 @@ public class SummaryFragment extends Fragment {
             public void onClick(View v) {
                 vp.setCurrentItem(1, true);
             }
+        });
+
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(view.getContext());
+        bottomSheetDialog.setContentView(R.layout.bottom_dialog_menu_detail);
+
+        btnShare = (Button) bottomSheetDialog.findViewById(R.id.btn_share);
+        btnSaveOffline = (Button) bottomSheetDialog.findViewById(R.id.btn_save_offline);
+        btnSeeUserRating = (Button) bottomSheetDialog.findViewById(R.id.btn_see_user_rating);
+
+
+        ibMore.setOnClickListener(v -> bottomSheetDialog.show());
+
+        btnShare.setOnClickListener(v -> {
+            // Create an Intent with the ACTION_SEND action
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+            shareIntent.setType("text/plain");
+
+            String shareText = receipt.toString();
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+
+            startActivity(Intent.createChooser(shareIntent, "Bagikan melalui"));
+            bottomSheetDialog.dismiss();
+        });
+
+
+        btnSaveOffline.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "Save Offline clicked", Toast.LENGTH_SHORT).show();
+            bottomSheetDialog.dismiss();
+        });
+
+        btnSeeUserRating.setOnClickListener(v -> {
+            bottomSheetDialog.dismiss();
+            Intent intent = new Intent(getContext(), RatingActivity.class);
+            getContext().startActivity(intent);
         });
 
         return view;
