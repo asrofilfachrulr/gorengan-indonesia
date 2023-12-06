5a536b9058fedeb95c8f20573cb71ac1da82e233
anya <riidloa@gmail.com>
2023-12-06 20:22:14 +0700

    reverting changes

--- app/src/main/java/com/example/gorenganindonesia/Activity/WelcomeActivity.java
+++ app/src/main/java/com/example/gorenganindonesia/Activity/WelcomeActivity.java
@@ -1,19 +1,12 @@
 package com.example.gorenganindonesia.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.example.gorenganindonesia.R;
 
@@ -21,9 +14,6 @@ public class WelcomeActivity extends AppCompatActivity {
     Button btnLogin, btnRegister;
     SharedPreferences sharedPreferences;
     boolean isOpened, isLogged;
-    private ImageView imageViewWelcome;
-    private int currentImageIndex = 0;
-    private int[] imageResources = {R.drawable.gorengan1, R.drawable.gorengan2, R.drawable.gorengan3, R.drawable.gorengan4, R.drawable.gorengan5}; // Ganti dengan sumber gambar Anda
 
 
     @Override
@@ -63,25 +53,6 @@ public class WelcomeActivity extends AppCompatActivity {
                 toLoginPage(true);
             }
         }
-        int colorGorengan = ContextCompat.getColor(this, R.color.gorengan);
-        int colorBlack = ContextCompat.getColor(this, R.color.black);
-
-        TextView colorText2 = (TextView)findViewById(R.id.judulapp);
-        SpannableString text2 = new SpannableString("GorenganIndonesia.");
-        text2.setSpan(new ForegroundColorSpan(colorGorengan), 0, 8, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        text2.setSpan(new ForegroundColorSpan(colorBlack), 8, 18, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//        colorText2.setText(text2);
-        colorText2.setText(text2, TextView.BufferType.SPANNABLE);
-
-        imageViewWelcome = findViewById(R.id.iv_hero);
-    }
-    public void changeImage(View view) {
-        if (currentImageIndex < imageResources.length - 1) {
-            currentImageIndex++;
-        } else {
-            currentImageIndex = 0;
-        }
-        imageViewWelcome.setImageResource(imageResources[currentImageIndex]);
     }
 
     public void toLoginPage(boolean dontBack){
